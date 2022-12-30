e1f236b6ba780f971e0d7d27b5832bfac40e30cc
Henrique Fan <fanhenrique@gmail.com>
2022-12-30 15:24:10 -0300

    Update: Button and Table

--- investment-wallet-web/src/components/Button.tsx
+++ investment-wallet-web/src/components/Button.tsx
@@ -1,8 +1,10 @@
+import {Plus} from 'phosphor-react';
 
 interface ButtonProps{
   text: string;
   onclick?: () => void;
   className?: string
+  plus?: boolean
 }
 
 export default function Button(props: ButtonProps){
@@ -10,11 +12,12 @@ export default function Button(props: ButtonProps){
     <button
       className={
         `h-9 w-96
+        flex justify-center items-center
         rounded text-white
         bg-green-800 hover:bg-green-900 
         ${props.className}`} 
       onClick={props.onclick}>
-        {props.text}
+        {props.plus ? <Plus className='mr-2' size={24} color="#ffffff" /> : (<></>)} {props.text}
     </button>
   );
 }
--- investment-wallet-web/src/components/Table/Table.tsx
+++ investment-wallet-web/src/components/Table/Table.tsx
@@ -1,4 +1,5 @@
 import {NotePencil, TrashSimple} from 'phosphor-react';
+import Button from '../Button';
 
 export interface ElementProps{
   id: number,
@@ -13,35 +14,36 @@ interface TableProps{
   list: ElementProps[];
 }
 
-
 export default function Table(props : TableProps){
   return(
-
-    <div className="table w-full border-collapse">
-      <div className="table-header-group">
-        <div className="table-row bg-transparent">
-          {props.header.map((h : string) => (
-            <div className="table-cell text-center" >
-              {h} 
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="table-row-group divide-y divide-zinc-200">
-        {props.list.map((l : ElementProps) => (
-          <div className="table-row bg-white">
-            <div className="table-cell text-center align-middle">{l.active}</div>
-            <div className="table-cell text-center align-middle">{l.currentPrice}</div>
-            <div className="table-cell text-center align-middle">{l.paidPrice}</div>
-            <div className="table-cell text-center align-middle">{l.variation}</div>
-            <div className="table-cell">
-              <div className="flex justify-center items-center">
-                <NotePencil size={24} color="#31A900" width='regular'/>
-                <TrashSimple size={24} color="#FF0000" width='regular' />
+    <div className='w-full h-full flex flex-col items-end'>
+      <Button plus className='mb-5 w-48' text='Adicionar ativo'></Button>
+      <div className="table w-full border-collapse">
+        <div className="table-header-group">
+          <div className="table-row bg-transparent">
+            {props.header.map((h : string) => (
+              <div className="table-cell text-center" >
+                {h} 
               </div>
-            </div>
-          </div>  
-        ))}  
+            ))}
+          </div>
+        </div>
+        <div className="table-row-group divide-y divide-zinc-200">
+          {props.list.map((l : ElementProps) => (
+            <div className="table-row bg-white">
+              <div className="table-cell text-center align-middle">{l.active}</div>
+              <div className="table-cell text-center align-middle">{l.currentPrice}</div>
+              <div className="table-cell text-center align-middle">{l.paidPrice}</div>
+              <div className="table-cell text-center align-middle">{l.variation}</div>
+              <div className="table-cell">
+                <div className="flex justify-center items-center">
+                  <NotePencil size={24} color="#31A900" width='regular'/>
+                  <TrashSimple size={24} color="#FF0000" width='regular' />
+                </div>
+              </div>
+            </div>  
+          ))}  
+        </div>
       </div>
     </div>
   );
