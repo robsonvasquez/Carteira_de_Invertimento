0356c8596df3568af280a93abb76f964ecfba166
Henrique Fan <fanhenrique@gmail.com>
2022-12-31 21:02:42 -0300

    Update: Select

--- investment-wallet-web/src/components/Select.tsx
+++ investment-wallet-web/src/components/Select.tsx
@@ -1,3 +1,6 @@
+import {CaretDown} from 'phosphor-react';
+import colors from 'tailwindcss/colors'
+
 interface optionInterface{
   value: string | number;
   key: string;
@@ -8,10 +11,13 @@ interface SelectProps{
 }
 export default function Select(props: SelectProps){
   return(
-    <select>
-      {props.options.map((op: optionInterface) => (
-        <option value={op.value}>{op.key}</option>
-      ))}
-    </select>
+    <div className='relative h-6 flex justify-end items-center'>
+      <select className="pl-2 pr-7 appearance-none bg-white border border-gray-300 hover:border-gray-500 rounded-lg shadow focus:outline-none">
+        {props.options.map((op: optionInterface) => (
+          <option className="" value={op.value}>{op.key}</option>
+        ))}
+      </select>
+      <CaretDown className='absolute mr-2' size={20} color={colors.black} />
+    </div>
   )
 }
