33d499418429da049ecff5bcae8c6228e134d472
Henrique Fan <fanhenrique@gmail.com>
2023-01-02 04:02:12 -0300

    Update: Animation arrow Select

--- investment-wallet-web/src/components/Select.tsx
+++ investment-wallet-web/src/components/Select.tsx
@@ -1,4 +1,5 @@
-import {CaretDown} from 'phosphor-react';
+import {CaretDown, CaretUp} from 'phosphor-react';
+import { useState } from 'react';
 import colors from 'tailwindcss/colors'
 
 interface optionInterface{
@@ -10,14 +11,26 @@ interface SelectProps{
   options: optionInterface[];
 }
 export default function Select(props: SelectProps){
+  const [caret, setCaret]  = useState(false)
+
   return(
     <div className='relative h-6 flex justify-end items-center'>
-      <select className="pl-2 pr-7 appearance-none bg-white border border-gray-300 hover:border-gray-500 rounded-lg shadow focus:outline-none">
+      <select 
+        className='
+          pl-2 pr-8 appearance-none bg-white border border-gray-300 
+        hover:border-gray-500 rounded-lg shadow focus:outline-none'
+        onPointerDown={e => setCaret(!caret)}
+        onPointerUp={e => setCaret(false)}
+        onPointerOut={e => setCaret(false)}
+      >
         {props.options.map((op: optionInterface) => (
-          <option className="" value={op.value}>{op.key}</option>
+          <option value={op.value}>{op.key}</option>
         ))}
       </select>
-      <CaretDown className='absolute mr-2' size={20} color={colors.black} />
+      {caret ?
+        (<CaretUp className='absolute mr-2 pointer-events-none' size={20} color={colors.black} />) :
+        (<CaretDown className='absolute mr-2 pointer-events-none' size={20} color={colors.black} />)
+      }
     </div>
   )
 }
