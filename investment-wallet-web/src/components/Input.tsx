18d68c972022a43767c990f3ba2d517cb7aef966
Henrique Fan <fanhenrique@gmail.com>
2023-01-04 02:17:04 -0300

    Update: Input interface extends InputHTMLAttributes

--- investment-wallet-web/src/components/Input.tsx
+++ investment-wallet-web/src/components/Input.tsx
@@ -1,42 +1,43 @@
+import { InputHTMLAttributes } from "react";
 import { useState } from "react";
 import { User, LockSimple, EnvelopeSimple, Eye, EyeSlash, Phone, IdentificationCard, Password } from "phosphor-react";
 
-interface InputProps {
-  value?: any
-  type?: 'text' | 'number' | 'password'
-  read?: boolean
-  placeholder: string
-  valueChange?: (value: any) => void
-  className?: string
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+  // name: string
+  // value?: any
+  // type?: 'text' | 'number' | 'password' | 'email'
+  // read?: boolean
+  // placeholder: string
+  // valueChange?: (value: any) => void
+  // className?: string
   image: string
 }
 
 export default function Input(props : InputProps){
+
+  // const[value, setValue] = useState(props.value);
+
+  const [showEye,setShowEye] = useState(props.type == 'password' ? true : false);
+  const [hiddenPassword,setHiddenPassword] = useState(showEye ? false : true);
+  const [type, setType] = useState(props.type);
 
   function passwordEye(type: 'text' | 'password'){
     setType(type);
     setHiddenPassword(!hiddenPassword);
   }
 
-  const [showEye,setShowEye] = useState(props.type == 'password' ? true : false);
-  const [hiddenPassword,setHiddenPassword] = useState(showEye ? false : true);
-  const [type, setType] = useState(props.type);
-  
   return(
     <div className="relative flex">
       <input
         className={
-          `h-10 w-96 p-1 mb-5 pl-9 
+          `h-10 w-96 p-1 mb-5 pl-9          
           placeholder-theme-color
           font-bold text-base 
           rounded-lg border-2 border-zinc-800 
-          focus:outline-none focus:ring-2 ring-green-800 focus:border-theme-color
-          ${props.className}`
+          focus:outline-none focus:ring-2 ring-green-800 focus:border-theme-color`
         }
-        value={props.value}
         type={type}
-        placeholder={props.placeholder}
-        onChange = {e => props.valueChange?.(e.target.value)}
+        {...props}
       />
       {props.image == 'user' ? (<User className="absolute ml-2 mt-2" size={24} color="#000000" weight="regular" />) : (<></>) }
      
