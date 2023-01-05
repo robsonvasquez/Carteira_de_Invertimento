2e8d68251dbf50acd8516320f903c1b7b72e36e1
Henrique Fan <fanhenrique@gmail.com>
2023-01-05 16:01:09 -0300

    Update: Profile position form

--- investment-wallet-web/src/pages/Profile.tsx
+++ investment-wallet-web/src/pages/Profile.tsx
@@ -7,7 +7,7 @@ export default function Profile(){
   return(
     <>
       <TopBar/>
-      <div className="h-screen flex justify-center items-center">
+      <div className="flex justify-center mt-20">
         <div className="w-96 flex flex-col">
           <h1 className={`text-theme-color text-center text-5xl font-bold mb-8`}>Minha conta</h1>
           <div className="flex flex-col justify-center items-center">
