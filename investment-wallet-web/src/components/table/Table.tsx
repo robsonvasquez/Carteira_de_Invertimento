7707c662dea16339eef464fa477fd0b8611c34ee
Henrique Fan <fanhenrique@gmail.com>
2023-01-08 11:05:18 -0300

    Update: Table

--- investment-wallet-web/src/components/table/Table.tsx
+++ investment-wallet-web/src/components/table/Table.tsx
@@ -18,28 +18,31 @@ import colors from 'tailwindcss/colors';
 export default function Table(props : TableProps){
   return(  
     
-      <table className='w-full rounded-xl font-semibold'>
-        <tr className="flex justify-around ">
+      <table className='w-full font-medium'>
+        <tr className="flex font-semibold px-1">
           {props.header.map((h : string) => (
-            <td className='text-center w-1/5'>{h}</td>
+            <th className='w-1/5 flex justify-center items-center'>{h}</th>
           ))}
         </tr>
-        
-          {props.list.map((l : ElementProps) => (
-            <tr className='bg-white flex border border-zinc-300 rounded-xl p-2 m-2'>
-              <td className='text-center w-1/5'>{l.active}</td>
-              <td className='text-center w-1/5'>{l.currentPrice}</td>
-              <td className='text-center w-1/5'>{l.paidPrice}</td>
-              <td className={`flex flex-row justify-center w-1/5 ${l.variation > 0 ? ('text-lime-800') : ('text-red-600')}`}> 
-                {l.variation > 0 ? <ArrowUp size={22} /> : <ArrowDown size={22} />} 
-                {Math.abs(l.variation)}%
-              </td>
-              <td className="flex justify-center gap-3 w-1/5">
-                <NotePencil size={25} color={colors.lime[800]}/>
-                <Trash size={25} color={colors.red[600]}/>
-              </td>
-            </tr>
-          ))}
+        <div className='flex flex-col gap-1'>
+        {props.list.map((l : ElementProps) => (
+          <tr className='bg-white flex border border-gray-300 rounded-lg p-1'>
+            <td className='w-1/5 flex justify-center items-center'>{l.active}</td>
+            <td className='w-1/5 flex justify-center items-center'>{l.currentPrice}</td>
+            <td className='w-1/5 flex justify-center items-center'>{l.paidPrice}</td>
+            <td className={`w-1/5 flex justify-start items-center ${l.variation > 0 ? ('text-lime-800') : ('text-red-600')}`}>
+              <div className='w-1/2 flex justify-end'>
+                {l.variation > 0 ? <ArrowUp size={20} /> : <ArrowDown size={20} />}
+              </div>
+              {Math.abs(l.variation)}%
+            </td>
+            <td className="flex justify-center gap-8 w-1/5">
+              <NotePencil size={25} color={colors.lime[800]}/>
+              <Trash size={25} color={colors.red[600]}/>
+            </td>
+          </tr>
+        ))}
+        </div>
       </table>
     
   );
