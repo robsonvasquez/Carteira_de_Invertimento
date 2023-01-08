8b54c035bffb39979185b144c341f5bfd8ea1c3b
Henrique Fan <fanhenrique@gmail.com>
2023-01-08 01:50:49 -0300

    Update: Select

--- investment-wallet-web/src/components/Select.tsx
+++ investment-wallet-web/src/components/Select.tsx
@@ -12,45 +12,52 @@ interface SelectProps{
 }
 export default function Select(props: SelectProps){
   
-  const [selected, setSelected] = useState(props.options[4].option)
+  const [selected, setSelected] = useState(props.options[4])
   const [caret, setCaret] = useState(false)
   const [showOptions, setShowOption] = useState(false)
 
-  console.log(selected);
   return(
-    <div className='relative h-9 w-32 flex flex-col justify-end items-center'>
+    <div className='relative h-9 w-32 flex flex-col'>
       <button 
         className='
-          flex flex-col items-center
-          w-full h-full bg-white border border-gray-300 
-        hover:border-gray-500 rounded-lg shadow '
+          w-full h-full flex flex-col items-center
+        bg-white border border-gray-300 
+        hover:border-gray-400 rounded-lg shadow'
         onClick={e => (setCaret(!caret), setShowOption(!showOptions))}        
         onBlur={e => (setCaret(false), setShowOption(false))}
       >
-        <div className='h-full w-full px-2 gap-2 flex items-center justify-between'>
-          <span>{selected}</span>
+        <div
+          className='
+            h-full w-full px-2           
+            flex items-center justify-between'
+        >
+          <span>{selected.option}</span>
           {caret ?
             (<CaretUp className='pointer-events-none' size={20} color={colors.black} />) :
             (<CaretDown className='pointer-events-none' size={20} color={colors.black} />)
           }
         </div>
-        <div className='absolute h-full w-full top-10 z-50'>
-          {showOptions ? 
-            (<div className='flex flex-col border text-justify border-zinc-300 bg-white rounded-lg p-1'>
+        <div className='absolute h-full w-full top-9 z-50'>
+          {showOptions ? (
+            <div 
+              className='
+                flex flex-col p-1
+                text-start bg-white leading-loose 
+                border border-gray-300 rounded-lg'
+            >
               {props.options.map((op: optionInterface) => (
-                <div className='pl-2 rounded hover:bg-green-200' onClick={e => (setSelected(op.option), setShowOption(false))} key={op.id}>{op.option}</div>
+                <div 
+                  className={`pl-2 rounded hover:bg-sky-100 ${selected.id == op.id ? 'bg-blue-200' : '' }`}
+                  onClick={e => (setSelected(op), setShowOption(false))} 
+                  key={op.id}
+                >
+                  {op.option}
+                </div>
               ))}
-            </div>) : 
-            (<></>)
+            </div>) : (<></>)
           }
         </div>
-
       </button>
-      
     </div>
   )
 }
-
-{/* {props.options.map((op: optionInterface) => (
-          <option key={op.id} value={op.value}>{op.option}</option>
-        ))} */}
