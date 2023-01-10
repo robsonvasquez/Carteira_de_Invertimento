57ada780c7cee6d6610aafe0ac168bde4e88d312
Robson <ribeirovasquez@gmail.com>
2023-01-10 20:50:34 -0300

    Update: Icon Profile

--- investment-wallet-web/src/components/Icons/IconProfile.tsx
+++ investment-wallet-web/src/components/Icons/IconProfile.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import colors from "tailwindcss/colors";
 import { CaretUp, Gear, SignOut, Triangle, UserCircle } from "phosphor-react";
-import { Link } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthProvider/useAuth";
 
 interface IconProfileProps{
   size: number;
@@ -13,37 +14,55 @@ export default function IconProfile(props: IconProfileProps){
   const [iconWeight, setIconWeigth] = useState(false)
   const [showOptions, setShowOptions] = useState(false)
 
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  function SingOut(){
+    auth.logout();
+
+    navigate('/login');
+  }
+
   return(
-    <button 
-      className={`relative rounded-full focus:outline-none ${props.className}`}
-      // onFocus={e => setIconWeigth(true)}
-      onClick={e => (setIconWeigth(!iconWeight), setShowOptions(!showOptions))}
-      onBlur={e => (setIconWeigth(false), setShowOptions(false))}
-    >
-      <UserCircle size={props.size} color={colors.white} weight={iconWeight ? 'regular' : 'thin' }/>
-        <div className={`
-          absolute -right-4 top-14 z-50
-          ${showOptions ? 'visible' : 'invisible hidden'}`}
-        >
-          <div className={`
-            flex flex-col p-1 
-            gap-1 bg-white 
-            border border-gray-300 rounded-lg`}
-          >
-            <Link className="w-full flex items-center justify-start gap-2 px-4 hover:bg-sky-100 rounded-lg" to="/profile">
-              <Gear size={24} color={colors.gray[700]} weight="regular" />
-              <span className="truncate text-black font-semibold text-lg leading-10">
-                Minha conta
-              </span>
-            </Link>
-            <Link className="w-full flex items-center justify-start gap-2 px-4 hover:bg-sky-100 rounded-lg" to="/login">
-              <SignOut size={24} color={colors.red[600]} weight="duotone" />
+    <button
+      onClick={e => (setIconWeigth(!iconWeight), setShowOptions(!showOptions))} 
+      onBlur={e => (setIconWeigth(!iconWeight), setShowOptions(false))} 
+      className="flex flex-col items-end" >
+
+      <UserCircle  
+        size={props.size} color={colors.white} weight={iconWeight ? 'regular' : 'thin' }        
+      />
+
+      {showOptions ? 
+        (
+          <div className="absolute top-16 rounded-md border border-gray-300">
+            <div onClick={e => navigate('/profile')} 
+              className="w-full flex items-center justify-start gap-2 px-4 hover:bg-sky-100 rounded-lg" >
+
+              <Gear className="pointer-events-none" 
+                size={24} 
+                color={colors.gray[700]} weight="regular" 
+              />
+
+                <span className="truncate text-black font-semibold text-lg leading-10">
+                  Minha conta
+                </span>
+
+            </div>
+
+            <div onClick={SingOut} className="w-full flex items-center justify-start gap-2 px-4 hover:bg-sky-100 rounded-lg">
+
+              <SignOut className="pointer-events-none" size={24} color={colors.red[600]} weight="duotone" />
+
               <span className="truncate text-red-600 font-semibold text-lg leading-10">
                 Sair
               </span>
-            </Link>
-        </div>
-      </div>
+
+            </div>
+
+          </div>
+
+        ) : (<></>)}
     </button>
   );
 }
