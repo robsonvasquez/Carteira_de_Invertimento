ab6947fdeba38e4386108d55026dbb6d209573b8
Robson <ribeirovasquez@gmail.com>
2023-01-10 01:58:11 -0300

    Update: CardResume

--- investment-wallet-web/src/components/cards/CardResume.tsx
+++ investment-wallet-web/src/components/cards/CardResume.tsx
@@ -1,19 +1,29 @@
-export default function Resume(){
+import { number } from "prop-types";
+
+interface CardResumeProps{
+  balance: number;
+  appliedValue: number;
+  title: string;
+  profitability: number;
+}
+
+export default function CardResume(props: CardResumeProps){
+
   return(
     <div className="w-2/5 my-5 p-4 bg-white border border-gray-300 rounded-lg">
-      <h1 className="mb-3 text-2xl">Resumo da carteira</h1>
+      <h1 className="mb-3 text-2xl">{props.title}</h1>
       <div className="flex justify-between">
         <div>
           <h2>Saldo</h2>
-          <h3>R$ 224,53</h3>
+          <h3>R$ {0 | props.balance}</h3>
         </div>
         <div>
           <h2>Valor aplicado</h2>
-          <h3>R$ 104,27</h3>
+          <h3>R$ {0 | props.appliedValue}</h3>
         </div>
         <div>
           <h2>Rentabilidade</h2>
-          <h3 className="text-lime-800">134%</h3>
+          <h3 className={props.profitability > 0 ? `text-lime-800` : `text-red-600`}>{0 | props.profitability}%</h3>
         </div>
       </div>
     </div>
