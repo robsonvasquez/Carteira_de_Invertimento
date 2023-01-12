413d97df14b8e91674ef2c71267ffa872056edb6
Henrique Fan <fanhenrique@gmail.com>
2023-01-11 21:25:19 -0300

    Update: CardWallet

--- investment-wallet-web/src/components/cards/CardWallet.tsx
+++ investment-wallet-web/src/components/cards/CardWallet.tsx
@@ -49,20 +49,20 @@ export default function CardWallet(props: CardWalletProps){
       mb-5 p-4 flex justify-between items-center
       bg-white border border-gray-300 rounded-lg"
     >
-      <div className="w-80 h-56 flex justify-center items-center">
+      <div className="w-56 h-56 flex justify-center items-center">
         <PieGraph data={data}/>
       </div>  
-      <div className="h-full pt-5 pb-10 flex flex-col justify-between">
+      <div className="h-32 w-32 flex flex-col justify-between ">
         <span className="flex items-center justify-center font-semibold">{props.title}</span>
 
-        <div className="w-36 flex justify-between">
-          <span className="flex items-center" >R$ {props.balance}</span>        
+        <div className="flex justify-between font-medium">
+          <span className="flex items-center truncate">R$ {props.balance}</span>        
           <div className="flex items-center">
             {props.variation > 0 ? 
               (<ArrowUp size={16} color={colors.lime[800]}/>):
               (<ArrowDown size={16} color={colors.red[600]}/>)
             }
-            <span className={`text-sm ${props.variation > 0 ? 'text-lime-800':'text-red-600'}`}>{Math.abs(props.variation)}</span>
+            <span className={`${props.variation > 0 ? 'text-lime-800':'text-red-600'}`}>{Math.abs(props.variation)}</span>
           </div>
         </div>
       </div>
