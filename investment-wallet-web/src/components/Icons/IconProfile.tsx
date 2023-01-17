c2668bc1a98ba5ad15ae108ce5601c25849a014d
Henrique Fan <fanhenrique@gmail.com>
2023-01-17 19:26:19 -0300

    Update: Rounded and remove outline

--- investment-wallet-web/src/components/Icons/IconProfile.tsx
+++ investment-wallet-web/src/components/Icons/IconProfile.tsx
@@ -25,43 +25,35 @@ export default function IconProfile(props: IconProfileProps){
 
   return(
     <button
+      className="flex flex-col items-end justify-center rounded-full focus:outline-none" 
       onClick={e => (setIconWeigth(!iconWeight), setShowOptions(!showOptions))} 
       onBlur={e => (setIconWeigth(false), setShowOptions(false))} 
-      className="flex flex-col items-end justify-center" >
-
+    >
       <UserCircle  
         size={props.size} color={colors.white} weight={iconWeight ? 'regular' : 'thin' }        
       />
-
       {showOptions ? 
         (
           <div className="absolute top-16 p-1 rounded-lg bg-white border border-gray-300">
             <div onClick={e => navigate('/profile')} 
               className="w-full flex items-center justify-start gap-2 px-4 hover:bg-sky-100 rounded-lg" >
-
               <Gear 
                 className="pointer-events-none" 
                 size={24} 
                 color={colors.gray[700]} weight="regular" 
               />
-
               <span className="truncate text-black font-semibold text-lg leading-10">
                 Minha conta
               </span>
-
             </div>
-
             <div
               onClick={SingOut} 
               className="w-full flex items-center justify-start gap-2 px-4 hover:bg-sky-100 rounded-lg"
             >
-
               <SignOut className="pointer-events-none" size={24} color={colors.red[600]} weight="duotone" />
-
               <span className="truncate text-red-600 font-semibold text-lg leading-10">
                 Sair
               </span>
-
             </div>
 
           </div>
