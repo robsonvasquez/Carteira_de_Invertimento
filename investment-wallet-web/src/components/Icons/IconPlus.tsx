2dd11c8ed5a81db15c5a5336b76362cd8f200231
Henrique Fan <fanhenrique@gmail.com>
2023-01-24 11:51:40 -0300

    Update: Modal register new wallet

--- investment-wallet-web/src/components/Icons/IconPlus.tsx
+++ investment-wallet-web/src/components/Icons/IconPlus.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import colors from "tailwindcss/colors";
 import { PlusCircle } from "phosphor-react";
+import { useNavigate }  from "react-router-dom";
+import Modal from "../modal/Modal";
+import Form from "../form/Form";
 import { formNewWallet } from "../../FormatForms";
 
 interface IconPlusProps{
@@ -11,30 +14,27 @@ interface IconPlusProps{
 export default function IconPlus(props: IconPlusProps){
 
   const [iconWeight, setIconWeigth] = useState(false)
-  const [showOptions, setShowOptions] = useState(false)
-  
+  const [showModal, setShowModal] = useState(false)
+
   return(
-    <button 
-      className={`relative rounded-full focus:outline-none ${props.className}`}
-      onFocus={e => setIconWeigth(!iconWeight)}
-      onClick={e => (setIconWeigth(!iconWeight), setShowOptions(!showOptions))}
-      onBlur={e => (setIconWeigth(false), setShowOptions(false))}
-    >
-      <PlusCircle size={props.size} color={colors.white} weight={iconWeight ? 'regular' : 'thin' }/>
-        <div className={`
-          absolute -right-4 top-14 z-50
-          ${showOptions ? 'visible' : 'invisible hidden'}`}
-        >
-          <div className={`
-            flex flex-col p-1 
-            gap-1 bg-white 
-            border border-gray-300 rounded-lg`}
-          >
-            <span className="truncate">ação 1</span>
-            <span className="truncate">ação 2</span>
-            <span className="truncate">ação 3</span>
-        </div>
-      </div>
-    </button>
+    <>
+      <button 
+        className={`rounded-full focus:outline-none ${props.className}`}
+        onClick={e => (setIconWeigth(!iconWeight), setShowModal(true))}
+        onBlur={e => setIconWeigth(false)}
+      >
+        <PlusCircle size={props.size} color={colors.white} weight={iconWeight ? 'regular' : 'thin' }/>      
+      </button>
+      
+      {showModal ? (
+        <Modal onClose={() => setShowModal(false)}>
+          <h1
+           className="text-theme-color text-center text-3xl sm:text-4xl font-bold mb-8" 
+          >Cadastrar um nova carteira</h1>
+          <Form inputs={formNewWallet} button="Cadastrar carteira"/>
+        </Modal>
+      ) : null}
+      
+    </>
   );
 }
