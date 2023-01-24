f894be0360be91f2262b682f308d8a557d1bfbf0
Henrique Fan <fanhenrique@gmail.com>
2023-01-24 11:51:11 -0300

    Update: New component Modal

---
+++ investment-wallet-web/src/components/modal/Modal.tsx
@@ -0,0 +1,40 @@
+import { MouseEvent, MouseEventHandler, ReactElement, useRef } from "react";
+
+interface ModalProps{
+  children: JSX.Element[];
+  onClose: () => void;
+}
+export default function Modal(props: ModalProps){
+  
+  const refBackgraound = useRef<HTMLDivElement>(null);
+  
+  const handleOutSideClick  = (event : MouseEvent<HTMLDivElement>)  => {
+    if (refBackgraound.current == event.target){
+      props.onClose()
+    }
+  }
+
+  return(
+    <div 
+      ref={refBackgraound}
+      className="
+        fixed w-screen h-screen z-40
+        flex items-center justify-center
+        top-0 left-0 cursor-pointer
+        bg-black bg-opacity-40"
+      onClick={handleOutSideClick}
+    >  
+      <div 
+        className="
+        relative max-w-max
+        flex flex-col
+        justify-center items-center
+        p-5 bg-white rounded-lg
+        cursor-auto border border-zinc-300"
+      >
+        {props.children}
+      </div>
+    </div>
+
+  );
+}
