fbb7278e5ae60f9a7470f6686dd610568ea46618
Henrique Fan <fanhenrique@gmail.com>
2023-01-26 17:59:12 -0300

    Update: Remove margin

--- investment-wallet-web/src/components/Icons/IconUser.tsx
+++ investment-wallet-web/src/components/Icons/IconUser.tsx
@@ -1,6 +1,6 @@
 export default function IconUser(){
     return(
-        <div className="w-full flex flex-col gap-y-3 text-center mb-20 text-white">
+        <div className="w-full flex flex-col gap-y-3 text-center text-white">
             <div className="flex justify-center duration-1000">
                 <img className="w-32 h-32 rounded-full" src="https://st.depositphotos.com/1269204/2716/i/450/depositphotos_27167511-stock-photo-smiling-middle-aged-man.jpg" alt="User" />
             </div>
