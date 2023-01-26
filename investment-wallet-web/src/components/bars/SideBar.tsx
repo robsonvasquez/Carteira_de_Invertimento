19890b2701de73af761b194d26be8362bf005624
Henrique Fan <fanhenrique@gmail.com>
2023-01-26 18:01:10 -0300

    Update: Close Side bar

--- investment-wallet-web/src/components/bars/SideBar.tsx
+++ investment-wallet-web/src/components/bars/SideBar.tsx
@@ -1,29 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import IconUser from "../Icons/IconUser"
 
-export default function SideBar(props:{open?: boolean}){
-    const navigate = useNavigate();
+export default function SideBar(props: {open: boolean}){
+  
+  const navigate = useNavigate();
 
-    return(
-        // <aside className={`${props.open ? "col-span-1" : "hidden"} row-span-3 felx justify-center bg-red-400`}>
-        //     <div className={`${!open ? "hidden" : "w-full"} flex flex-col items-center p-4`}>
-        //         <div>Carteiras</div>
-        //         <div>Minha Conta</div>
-        //         <div>Carteiras</div>
-        //         <div>Minha Conta</div>
-        //     </div>
-        // </aside>
-
-    <aside className={`${props.open ? "w-[50%] sm:w-[50%] md:w-[25%]" : "w-0"} duration-300 bg-theme-color py-5`}>
-        <div className={`${props.open ? "visible" : "invisible"} flex flex-col gap-4 items-center p-4`}>
-            <IconUser/>
-            <div className="w-full gap-y-1 sm:gap-y-4 flex flex-col text-center text-white">
-                <div onClick={e => navigate('/login')} className="w-full rounded-md py-4 hover:bg-theme-hover-color border-y-2">Carteiras</div>
-                <div className="w-full rounded-md py-4 hover:bg-theme-hover-color border-y-2">Minha Conta</div>
-                <div className="w-full rounded-md py-4 hover:bg-theme-hover-color border-y-2">Carteiras</div>
-                <div className="w-full rounded-md py-4 hover:bg-theme-hover-color border-y-2">Minha Conta</div>
-            </div> 
-        </div>
+  return(
+    <aside className={`${props.open ? "w-[50%] sm:w-[50%] md:w-[25%]" : "w-0"} transform ease-linear duration-200 bg-theme-color`}>
+      <div className={`${props.open ? "visible" : "invisible"} flex flex-col gap-20 items-center pt-5 px-4`}>
+        <IconUser/>
+        <div className="w-full gap-y-1 sm:gap-y-4 flex flex-col text-center text-white">
+          <div onClick={e => navigate('/wallet')} className="w-full rounded-md py-4 hover:bg-theme-hover-color border-y-2">Carteiras</div>
+          <div className="w-full rounded-md py-4 hover:bg-theme-hover-color border-y-2">Minha Conta</div>
+          <div className="w-full rounded-md py-4 hover:bg-theme-hover-color border-y-2">Carteiras</div>
+          <div className="w-full rounded-md py-4 hover:bg-theme-hover-color border-y-2">Minha Conta</div>
+        </div> 
+      </div>
     </aside>
-    );
+  );
 }
--- investment-wallet-web/src/components/layout/Layout.tsx
+++ investment-wallet-web/src/components/layout/Layout.tsx
@@ -1,35 +1,38 @@
-import { useState } from "react";
+import { MouseEvent, useState, useRef } from "react";
 import BottomBar from "../bars/BottomBar";
 import Sidebar from "../bars/SideBar";
 import TopBar from "../bars/TopBar";
 
 export default function Layout(props: {children: JSX.Element}){
 
-    const[open, setOpen] = useState(true);
+  const refInside = useRef<HTMLDivElement>(null);
+  
+  const[open, setOpen] = useState(false);
+  
+  const handleInsideClick = (event: MouseEvent<HTMLDivElement>) => {
+    if (refInside.current == event.target) setOpen(false)
+  }
 
-    function onclickTopBar(){
-        setOpen(!open);
-        console.log("deu");
-    }
-    return(
-        // <div className="w-screen h-screen grid grid-cols-4 grid-rows-3">
-        //     <Sidebar open={!open}/>
-        //     <div className={`${open ? "col-span-4" : "col-span-3"} row-span-3 grid content-between`}>
-        //         <TopBar onclick={onclickTopBar}/>
-        //         {children}
-        //         <BottomBar/>
-        //     </div>
-        // </div>
-        <div className="w-full flex">
-            <Sidebar open={!open}/>
-            <div 
-              className="w-full flex flex-col 
-              scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-gray-400"
-            >                
-              <TopBar onclick={onclickTopBar}/>
-              {props.children}
-              <BottomBar/>
-            </div>
+  return(
+    <div className="w-full flex">
+      <Sidebar open={open}/>
+      <div 
+        ref={refInside}
+        className="
+          w-full
+          bg-transparent"
+        onClick={handleInsideClick}
+      >
+        <div
+          className={`w-full flex flex-col 
+            ${open ? 'pointer-events-none': ''}
+            scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-gray-400`}
+        >                
+          <TopBar open={open} onOpen={() => setOpen(true)}/>
+          {props.children}
+          <BottomBar/>
         </div>
-    );
+      </div>   
+    </div>
+  );
 }
