8c072b8c31387856e9adcb41a07735f508f9ef54
Henrique Fan <fanhenrique@gmail.com>
2023-01-26 18:17:11 -0300

    Update: IconUser

--- investment-wallet-web/src/components/Icons/IconUser.tsx
+++ investment-wallet-web/src/components/Icons/IconUser.tsx
@@ -1,12 +1,12 @@
 export default function IconUser(){
-    return(
-        <div className="w-full flex flex-col gap-y-3 text-center text-white">
-            <div className="flex justify-center duration-1000">
-                <img className="w-32 h-32 rounded-full" src="https://st.depositphotos.com/1269204/2716/i/450/depositphotos_27167511-stock-photo-smiling-middle-aged-man.jpg" alt="User" />
-            </div>
-            <div className="font-semibold text-2xl sm:text-3xl">
-                <h2>Henrique Fan</h2>
-            </div>
-        </div>
-    );
+  return(
+    <div className="w-full flex flex-col gap-3 text-center">
+      <div className="flex justify-center items-center">
+        <img className="w-28 h-28 rounded-full" src="https://st.depositphotos.com/1269204/2716/i/450/depositphotos_27167511-stock-photo-smiling-middle-aged-man.jpg" alt="User" />
+      </div>
+      <div className=" font-semibold text-2xl sm:text-3xl text-white">
+        <h2>"Nome do usuario"</h2>
+      </div>
+    </div>
+  );
 }
