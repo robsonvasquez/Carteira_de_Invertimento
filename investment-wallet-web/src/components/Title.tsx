2903cc3fc5fb468384d8bda9dc962d502154a0cd
Henrique Fan <fanhenrique@gmail.com>
2023-01-26 18:53:54 -0300

    Update: Remove margin

--- investment-wallet-web/src/components/Title.tsx
+++ investment-wallet-web/src/components/Title.tsx
@@ -1,16 +1,13 @@
 import { HTMLAttributes } from "react";
 
-interface TitleProps extends HTMLAttributes<HTMLHeadingElement> {}
-
-export default function Title(props: TitleProps){
+export default function Title(props: HTMLAttributes<HTMLHeadingElement>){
   
   return(
     <h1
       className={`
-      mb-5
       text-theme-color text-center 
       text-2xl sm:text-3xl 
-      font-semibold
+      font-bold
       ${props.className}`}
     >
       {props.children}
