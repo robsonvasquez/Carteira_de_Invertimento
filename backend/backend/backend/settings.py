07929147e8899f104f649b33db00f1b2a3e21813
Henrique Fan <fanhenrique@gmail.com>
2023-02-03 16:15:32 -0300

    Update: Installed app user

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'apps.wallet',
     'apps.active',
+    'apps.user',
 ]
+
+AUTH_USER_MODEL = 'user.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
