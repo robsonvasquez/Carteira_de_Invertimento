909cf29d8ee3c8dad3fc997e23c968bcd630f740
Henrique Fan <fanhenrique@gmail.com>
2023-02-04 00:53:21 -0300

    Update: model active

--- backend/backend/apps/active/models.py
+++ backend/backend/apps/active/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinValueValidator, DecimalValidator
+from django.core.validators import MinValueValidator
 # from apps.wallet.models import Wallet
 
 class Active(models.Model):
@@ -8,10 +8,10 @@ class Active(models.Model):
   data_purchase = models.DateField()
   institution = models.CharField(max_length=100, default='', blank=True)
   amount = models.IntegerField(default=0, validators=[MinValueValidator(0)], blank=False)
-  price_paid = models.DecimalField(default=0.0, validators=[MinValueValidator(0.0)], max_digits=10, decimal_places=2, blank=False)
-  invested_price = models.DecimalField(default=0.0, validators=[MinValueValidator(0.0)], max_digits=10, decimal_places=2, blank=False)
+  price_paid = models.DecimalField(default=0.0, validators=[MinValueValidator(0.0)], max_digits=20, decimal_places=2, blank=False)
+  invested_price = models.DecimalField(default=0.0, validators=[MinValueValidator(0.0)], max_digits=20, decimal_places=2, blank=False)
   # rever taxa de corretagem
-  brokerage_rate = models.DecimalField(default=0.0, validators=[MinValueValidator(0.0)], max_digits=10, decimal_places=2, blank=True)
+  brokerage_rate = models.DecimalField(default=0.0, validators=[MinValueValidator(0.0)], max_digits=20, decimal_places=2, blank=True)
   create_date = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
