40b4e3d4f6bea8183a99939052f877eb29dedd5f
Henrique Fan <fanhenrique@gmail.com>
2023-02-07 21:11:00 -0300

    Update: New model User

--- backend/backend/apps/user/models.py
+++ backend/backend/apps/user/models.py
@@ -1,6 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import EmailValidator
+from django.contrib.auth.validators import UnicodeUsernameValidator
+
 class User(AbstractUser):
-  pass
+  username = models.CharField(max_length=150, validators=[UnicodeUsernameValidator()], unique=False, blank=True)
+  cpf = models.CharField(max_length=11, unique=True, blank=True)
+  email = models.EmailField(validators=[EmailValidator], unique=True)
+  is_trusty = models.BooleanField(default=False, help_text="Designates whether this user has confirmed his account")
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['username']
