cf119069b9867ac90ca4e434aa5293d71db5d873
Henrique Fan <fanhenrique@gmail.com>
2023-02-22 21:55:34 -0300

    Update: Rename class

--- backend/backend/apps/notification/api/viewsets.py
+++ backend/backend/apps/notification/api/viewsets.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from apps.notification.api.serializers import NotificationSerializer
-from apps.notification.models import Nofification
+from apps.notification.models import Notification
 
 class NotificationViewSet(ModelViewSet):
-  queryset = Nofification.objects.all()
+  queryset = Notification.objects.all()
   serializer_class = NotificationSerializer
